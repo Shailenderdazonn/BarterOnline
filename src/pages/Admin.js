3875b5d45109d14571442ef2af451a0e21eb3713
Shailenderdazonn <115336729+Shailenderdazonn@users.noreply.github.com>
2023-02-10 17:00:48 +0530

    Update Admin.js

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -6,6 +6,25 @@ import styles from "./Admin.module.css";
 const Admin = () => {
   const navigate = useNavigate();
 
+  function validation() {
+    var id = document.f1.name.value;
+    var ps = document.f1.pass.value;
+    if (id.length == "" && ps.length == "") {
+      alert("User email and Password fields are empty");
+      return false;
+    }
+    else {
+      if (id.length == "") {
+        alert("User Email is empty");
+        return false;
+      }
+      if (ps.length == "") {
+        alert("Password field is empty");
+        return false;
+      }
+    }
+  }
+
   const onBO51Click = useCallback(() => {
     navigate("/");
   }, [navigate]);
@@ -20,7 +39,7 @@ const Admin = () => {
         />
       </div>
       <div className={styles.featuredDestinationBlurb}>
-        <form className={styles.form}>
+        <form className={styles.form}  name="f1" action="http://localhost/Classifieds%20ad/src/API/admin.php" onSubmit={validation} method="POST">
           <a className={styles.bo51} onClick={onBO51Click} />
           <div className={styles.formFields}>
             <div className={styles.mobile}>
@@ -30,7 +49,8 @@ const Admin = () => {
                 variant="outlined"
                 type="text"
                 label="Enter Email/Mobile No."
-                placeholder="Placeholder"
+                name="email"
+                id="email"                
                 size="medium"
                 margin="none"
               />
@@ -42,12 +62,15 @@ const Admin = () => {
                 variant="outlined"
                 type="text"
                 label="Enter Email/Mobile No."
-                placeholder="Placeholder"
+                name="password"
+                id="password"
                 size="medium"
                 margin="none"
               />
             </div>
-            <button className={styles.formSubmitButton}>
+            <button className={styles.formSubmitButton} type="submit"
+             id="btn"
+             value="Login" >
               <div className={styles.submit}>Submit</div>
             </button>
           </div>
